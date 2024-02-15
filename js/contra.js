535cd45a5f34c19975e87e405fbc54be8d142686
RodrickMJ <231199@ids.upchiapas.edu.mx>
2024-02-14 23:16:41 -0600

    Primer Commit

--- js/contra.js
+++ js/contra.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const audio = document.getElementById("myAudio");
-    const playPauseBtn = document.getElementById("playPauseBtn");
-  
-    playPauseBtn.addEventListener("click", function() {
+  const audio = document.getElementById("myAudio");
+  const playPauseBtn = document.getElementById("playPauseBtn");
+
+  playPauseBtn.addEventListener("click", function() {
       if (audio.paused) {
-        audio.play();
+          console.log("Reproduciendo");
+          audio.play();
       } else {
-        audio.pause();
+          console.log("En pausa");
+          audio.pause();
       }
-    });
   });
-  
+});
